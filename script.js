795a693d6efdb6e5f25ebf4cdc801225ad5479a5
AlecVCodes <alectvine@gmail.com>
2025-10-21 15:05:10 +1000

    final commit
    Added the finishing touches to the tip calculator.
    
    Responsive design and custom input button and functionality are now complete

--- script.js
+++ script.js
@@ -17,20 +17,14 @@ let userInputs = [
   selectedTip,
 ];
 
+// get all the tip buttons
 const selectedTipsArray = [...document.querySelectorAll(".tip-option")];
 
-//Add active class
-selectedTipsArray.forEach((tip) => {
-  tip.addEventListener("click", activeClassToggle);
-});
+// custom tip button
 
-function activeClassToggle(e) {
-  selectedTipsArray.forEach((tip) => {
-    tip.classList.remove("active");
-  });
+const customTipInput = document.querySelector("#custom-input");
 
-  e.currentTarget.classList.add("active");
-}
+console.log(customTipInput);
 // ResultsValues
 
 const TotalTipResult = document.querySelector(".total-tip-amount-number");
@@ -39,13 +33,6 @@ const totalResult = document.querySelector(".total-number");
 //Reset Values button
 
 const resetValuesBtn = document.querySelector(".reset-btn");
-
-// reset the bill text to 0 on load
-
-window.onload = function () {
-  billInputField.value = "";
-  numberOfPeopleInputField.value = "";
-};
 
 // listen for bill input changes
 billInputField.addEventListener("input", onBillAmountChange);
@@ -65,6 +52,10 @@ selectedTipsArray.forEach((tipButton) => {
   tipButton.addEventListener("click", (e) => onTipChange(e));
 });
 
+// handle click for custom tip
+
+customTipInput.addEventListener("input", (e) => onTipChange(e));
+
 function onBillAmountChange(e) {
   //ensures decimal places
   userInputtedBillValue = parseFloat(e.target.value);
@@ -76,23 +67,29 @@ function onBillAmountChange(e) {
   return userInputtedBillValue;
 }
 
+// run this function when change to Number of People Input
 function onPeopleChange(e) {
   userInputtedNumberOfPeopleValue = parseInt(e.target.value);
 
   userInputs[1] = userInputtedNumberOfPeopleValue;
+
+  cantBeZero(userInputtedNumberOfPeopleValue);
 
   checkAllInputs();
 
   return userInputtedNumberOfPeopleValue;
 }
 
+//run this function when tip value changes
 function onTipChange(e) {
-  selectedTip = parseFloat(e.target.innerText);
+  //This either detects the text from the percentage buttons or the user inputted value
+  selectedTip = parseFloat(e.target.innerText || e.target.value);
 
   userInputs[2] = selectedTip;
   checkAllInputs();
 }
 
+//calculate final tip total
 function calculateTip() {
   let finalTipAmount = parseFloat(
     userInputtedBillValue * (userInputs[2] / 100).toFixed(2)
@@ -101,9 +98,10 @@ function calculateTip() {
   totalPerPerson(finalTipAmount);
 }
 
+// calculate total amount and total tip amount per person
 function totalPerPerson(finalTip) {
   let totalAmountPerPerson =
-    userInputtedBillValue + finalTip * userInputtedNumberOfPeopleValue;
+    (userInputtedBillValue + finalTip) / userInputtedNumberOfPeopleValue;
 
   let totalTipAmountPerPerson = parseFloat(
     finalTip / userInputtedNumberOfPeopleValue
@@ -112,7 +110,7 @@ function totalPerPerson(finalTip) {
   updateResults(totalTipAmountPerPerson, totalAmountPerPerson);
 }
 
-// only calculate the tip when all inputs aren't 0
+// function checks if all values in userInputs aren't 0 and then runs the function to calculate the tip and total
 function checkAllInputs() {
   console.log("checking all inputs");
 
@@ -122,11 +120,15 @@ function checkAllInputs() {
   );
 
   if (allInputsValid) {
-    console.log(userInputs[2], "completed user inputs");
     calculateTip();
   } else return;
 }
-
+//
+//
+//
+//
+//
+//
 //UI
 
 //update total results UI
@@ -136,17 +138,34 @@ function updateResults(totalTipAmountPerPerson, totalAmountPerPerson) {
     TotalTipResult.textContent = `$${totalTipAmountPerPerson.toFixed(2)}`;
     totalResult.textContent = `$${totalAmountPerPerson.toFixed(2)}`;
   }
+
+  //make reset button enabled
+  resetValuesBtn.classList.remove("disabled");
 }
 
 // Reset UI
+
+//Reset UI on page load
+// reset user inputs
+window.onload = function () {
+  billInputField.value = "";
+  customTipInput.value = "";
+  numberOfPeopleInputField.value = "";
+};
+
+//Reset UI on Click
 
 resetValuesBtn.addEventListener("click", resetUI);
 
 console.log(resetValuesBtn);
 
 function resetUI() {
+  // reset the rest button back to disabled
+  resetValuesBtn.classList.add("disabled");
+
   //reset user inputs
   billInputField.value = "";
+  customTipInput.value = "";
   numberOfPeopleInputField.value = "";
 
   selectedTipsArray.forEach((tip) => {
@@ -155,4 +174,30 @@ function resetUI() {
   //reset results
   TotalTipResult.textContent = "$0.00";
   totalResult.textContent = "$0.00";
+}
+
+//Add active class to options
+selectedTipsArray.forEach((tip) => {
+  tip.addEventListener("click", activeClassToggle);
+});
+
+function activeClassToggle(e) {
+  selectedTipsArray.forEach((tip) => {
+    tip.classList.remove("active");
+  });
+
+  e.currentTarget.classList.add("active");
+}
+
+// function to make sure user doesn't make people number 0
+
+function cantBeZero(userInput) {
+  if (userInput === 0) {
+    document
+      .querySelector("#number-of-people-wrapper")
+      .classList.add("rejected");
+  } else
+    document
+      .querySelector("#number-of-people-wrapper")
+      .classList.remove("rejected");
 }
